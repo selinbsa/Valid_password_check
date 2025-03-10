fed74a2ca1eb15307afd6a9ea4646c73de41e838
selinbsa <selinbasakagan@gmail.com>
2025-03-11 01:53:56 +0300

    Adding comments

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,20 @@ public class Main {
 
         boolean isValid = false;
 
+        //Asking password until valid password
         while (!isValid) {
 
+            //Taking password
             System.out.println("Welcome to the system. To continue, please enter your password: ");
             String password = scanner.nextLine();
 
-
+            //Checking length of password
             if (password.length() < 8) {
                 System.out.println("Invalid Password! Try again.");
                 continue;
             }
 
-
+            //Checking space
             for (int i = 0; i < password.length(); i++) {
                 if (password.charAt(i) == ' ') {
                     System.out.println("Invalid Password! Try again.");
@@ -25,6 +27,7 @@ public class Main {
                 }
             }
 
+            //Checking first character of password
             char firstCharacter = password.charAt(0);
             if (Character.isLowerCase(firstCharacter)) {
                 System.out.println("Invalid Password! Try again.");
@@ -32,13 +35,14 @@ public class Main {
             }
 
 
+            //Checking last character of password
             char lastCharacter = password.charAt(password.length() - 1);
             if (lastCharacter != '?') {
                 System.out.println("Invalid Password! Try again.");
                 continue;
             }
 
-
+            //Showing valid result
             System.out.println("Valid Password! Welcome.");
             isValid = true;
         }
